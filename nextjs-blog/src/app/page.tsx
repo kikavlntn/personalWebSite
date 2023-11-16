5476948c212d927320c5a033c963db8ffcecc64a
kika <franfonsecav@gmail.com>
2023-11-16 18:31:08 +0100

    just trying

--- nextjs-blog/src/app/page.tsx
+++ nextjs-blog/src/app/page.tsx
@@ -1,10 +1,13 @@
 export default function Home() {
 	return (
 		<main className="h-screen bg-slate-900 leading-relaxed text-slate-400">
-			<div className="text-white text-4xl font-Mosk">Francisca Fonseca</div>
 			<div>
-				Front-end <br></br>Web developer
+				<div className="text-white text-4xl font-Mosk">Francisca Fonseca</div>
+				<div>
+					Front-end <br></br>Web developer
+				</div>
 			</div>
+			<div></div>
 		</main>
 	);
 }
